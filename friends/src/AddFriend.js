bd6125c9a79bf71c29c62916d76a45e7526cca67
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-18 06:01:54 +0700

    fixed refresh bug and styling is complete

--- friends/src/AddFriend.js
+++ friends/src/AddFriend.js
@@ -14,23 +14,34 @@ class AddFriend extends Component {
         // e.preventDefault();
         this.setState({
             [e.target.name]: e.target.value,
-            [e.target.age]: e.target.value,
-            [e.target.email]: e.target.value
+            [e.target.name]: e.target.value,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleClick = e => {
+        this.props.createFriend(this.state);
+        this.setState({
+            name: '',
+            age: '',
+            email: ''
         });
     };
 
     render() {
         return (
             <div>
-                <form>
-                    <input onChange={this.handleFriendInput} value={this.props.name} name='name' placeholder="Name" />
-                    <input onChange={this.handleFriendInput} value={this.props.age} name='age' placeholder="Age" />
-                    <input onChange={this.handleFriendInput} value={this.props.email} name='email' placeholder="Email" />
-                    <button onClick={() => createFriend(this.state)}>Add Friend</button>
-                </form>
+                <div className="form">
+                    <input onChange={this.handleFriendInput} value={this.state.name} name='name' placeholder="Name" />
+                    <input onChange={this.handleFriendInput} value={this.state.age} name='age' placeholder="Age" />
+                    <input onChange={this.handleFriendInput} value={this.state.email} name='email' placeholder="Email" />
+                    <button onClick={this.handleClick}
+                    >Add Friend
+                    </button>
+                </div>
             </div>
         )
     }
 }
 
-export default connect(null, {createFriend})(AddFriend);
+export default connect(null, { createFriend })(AddFriend);
